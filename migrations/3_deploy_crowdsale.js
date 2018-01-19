92fa3fd0d2720f1016f2d6ff0be66a9b9458e025
Wesley <wesley@vanheije.net>
2018-01-19 10:18:52 +0000

    updated deployer

--- migrations/3_deploy_crowdsale.js
+++ migrations/3_deploy_crowdsale.js
@@ -13,14 +13,5 @@ module.exports = function(deployer, network, accounts) {
     
     // Deploying..
     deployer.link(SafeMath, BlockportCrowdsale);
-    deployer.then(function() {
-        return deployer.deploy(BlockportCrowdsale, settings.crowdsaleCap, settings.crowdsaleStartTimestamp, settings.crowdsaleEndTimestamp, crowdsaleRate, settings.companyFundWallet, BlockportToken.address, settings.companyTokenVault, settings.companyTokenVault);
-    }).then(function() { 
-        console.log("BlockportCrowdsale deployed: " + BlockportCrowdsale.address);
-        return BlockportToken.deployed();
-    }).then(function(instance) { 
-        console.log("Token instance: " + instance.address);
-        console.log("Transfering ownership...");
-        return instance.transferOwnership(BlockportCrowdsale.address, {from: admin, gas: 35000});
-    });
+    deployer.deploy(BlockportCrowdsale, settings.crowdsaleCap, settings.crowdsaleStartTimestamp, settings.crowdsaleEndTimestamp, crowdsaleRate, settings.companyFundWallet, BlockportToken.address, settings.companyTokenVault, settings.companyTokenVault);
 }
